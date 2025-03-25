bd227950dcc3ba66f1b2ec6d000e9e6cae59912e
Nicolás Casmuz <nicolascasmuz@gmail.com>
2025-03-25 06:59:58 -0300

    commit-001

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,23 @@
-import { Sequelize } from "sequelize";
+/* import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(process.env.NEON_STRING, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+export { sequelize }; */
+
+import { Sequelize } from "sequelize";
+import pg from "pg";
+
+const sequelize = new Sequelize(process.env.NEON_STRING, {
+  dialect: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
